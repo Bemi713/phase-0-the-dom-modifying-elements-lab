f76d482c186028cf12b70b9a1813e0078f713d6c
Brian <Bemi713@gmail.com>
2022-02-07 23:59:30 -0600

    commit tree c24718959bca95aeb45c33c6582f907e83fa7d1c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// Write your code here
+var main = document.getElementById("main")
+main.remove();
+document.head.append(H1)
+
+let newHeader = 'h1#victory'
+document.append.head('newHeader')
+console.log();
